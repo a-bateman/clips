a66fefe93d226fa877742a4db8c9bf7553f7e10f
wittmaxi@outlook.de <wittmaxi@outlook.de>
2018-12-04 20:04:31 +0100

    Added collor support to lfs

--- src/lfs/lfs.cpp
+++ src/lfs/lfs.cpp
@@ -6,14 +6,16 @@
 
 #include <string>
 #include <iostream>
-#include <filesystem>
+#include <experimental/filesystem>
 #include <string>
 #include <commander.hpp>
 #include <exception>
 
+
+std::string getDatatypeColorCode (const std::experimental::filesystem::directory_entry& entry);
+
 inline bool shouldDisplayHelp (const CMD::commander &args) {
     return(args.isFlagSet("help") || args.isFlagSet("info"));
-
 }
 
 inline bool helpMessage(const CMD::commander &args)
@@ -36,12 +38,18 @@ inline void inputDirectoryName (std::string& dir) {
 }
 
 
+std::string extractName (const std::string name) {
+    return name.substr (2, name.size());
+}
+
 inline void printFiles(std::string dir) {
-    namespace FS = std::filesystem;
-    for(std::string filename : FS::directory_iterator(dir))
-    {
-        std::cout << filename << std::endl;
-    }
+    namespace FS = std::experimental::filesystem;
+    for(auto filename : FS::directory_iterator(dir))
+        try {
+            std::cout << getDatatypeColorCode(filename) << extractName(filename.path()) << "\e[0m\n";
+        } catch (const std::exception& ex) {
+            std::cout << ex.what() << "\n";
+        }
 }
 
 int main(int argc, char** argv)
@@ -52,11 +60,60 @@ int main(int argc, char** argv)
     if(helpMessage(args))
         return 0;
 
-    if(args.getFlagCount() < 1)
-        inputDirectoryName(dir);
+    if(args.getFlagCount() == 0)
+        dir = ".";
     else
         dir = args.getAllFlagsUnlike(std::regex ("-\\w*"))[0];
 
     printFiles(dir);
     return 0;
+}
+
+
+// The codes for foreground and background colours are:
+
+//          foreground background
+// black        30         40
+// red          31         41
+// green        32         42
+// yellow       33         43
+// blue         34         44
+// magenta      35         45
+// cyan         36         46
+// white        37         47
+
+// Additionally, you can use these:
+
+// reset             0  (everything back to normal)
+// bold/bright       1  (often a brighter shade of the same colour)
+// underline         4
+// inverse           7  (swap foreground and background colours)
+// bold/bright off  21
+// underline off    24
+// inverse off      27
+
+//source: https://stackoverflow.com/questions/2616906/how-do-i-output-coloured-text-to-a-linux-terminal
+
+std::string getDatatypeColorCode (const std::experimental::filesystem::directory_entry& entry) {
+    switch (entry.status().type()) {
+        case std::experimental::filesystem::file_type::block:
+            return "\e[30;47m";
+        case std::experimental::filesystem::file_type::character:
+            return "\e[31;47m";
+        case std::experimental::filesystem::file_type::not_found:
+            return "\e[37;41m";
+        case std::experimental::filesystem::file_type::regular:
+            return "\e[0m";
+        case std::experimental::filesystem::file_type::directory:
+            return "\e[4m\e[7m";
+        case std::experimental::filesystem::file_type::symlink:
+            return "\e[4m";
+        case std::experimental::filesystem::file_type::fifo:
+            return "\e[35;40m";
+        case std::experimental::filesystem::file_type::socket:
+            return "\e[32;40m";
+        case std::experimental::filesystem::file_type::unknown:
+            return "\e[34;47m";
+    }
+    return "";
 }
